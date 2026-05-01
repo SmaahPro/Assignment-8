94cd5ce89ed091a702df5e029b5b5a85acb05b2e
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-05-01 18:22:41 +0600

    ExtraSectionHome

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import Footer from "@/components/shared/Footer";
+import ExtraSection from "@/components/homepage/ExtraSection";
 
 export default function Home() {
-    return (
-        <div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <ExtraSection />
+      <Footer />
+    </div>
+  );
 }
