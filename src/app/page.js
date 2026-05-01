2029f67920c59912e84031b915c07c657a785a7a
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-05-01 21:40:55 +0600

    NavbarSection

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 import ExtraSection from "@/components/homepage/ExtraSection";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <ExtraSection />
       <Footer />
     </div>
