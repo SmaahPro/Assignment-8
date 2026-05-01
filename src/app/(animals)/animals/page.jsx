5633ab2677f6a4fea535579afbfc2bbe102d725e
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-05-02 00:36:14 +0600

    AddFooter&NavbarInAllAnimalsPage

--- src/app/(animals)/animals/page.jsx
+++ src/app/(animals)/animals/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 
 const AllAnimals = () => {
     return (
         <div>
-            AllAnimals
+            <Navbar />
+            <h1 className="text-center">All Animals</h1>
+            <Footer />
         </div>
     );
 };
---
+++ src/components/homepage/FeaturedAnimals.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedAnimals = () => {
+    return (
+        <div className='text-center py-5'>
+            Here FeaturedAnimals
+        </div>
+    );
+};
+
+export default FeaturedAnimals;
