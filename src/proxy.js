5ec8c4d178ccf5d37446deb28e473cb9cb28532f
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-05-04 13:59:40 +0600

    UpdateProxyJs

--- src/proxy.js
+++ src/proxy.js
@@ -9,30 +9,15 @@ export async function proxy(request) {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
+    console.log(session, "session");
 
-    const { pathname } = request.nextUrl;
-
-    const protectedRoutes = ["/my-profile", "/update-profile", "/details-page/:path*"];
-
-    const isProtectedRoute = protectedRoutes.some(route => {
-        if (route.includes(":path*")) {
-            const baseRoute = route.replace("/:path*", "");
-            return pathname.startsWith(baseRoute);
-        }
-        return pathname === route;
-    });
-
-    if (!session && isProtectedRoute) {
-        return NextResponse.redirect(new URL("/login", request.url));
+    if (session) {
+        return NextResponse.next();
     }
 
-    if (session && (pathname === "/login" || pathname === "/register")) {
-        return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    return NextResponse.next();
+    return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
-    matcher: ["/my-profile", "/update-profile", "/details-page/:path*", "/login", "/register"],
+    matcher: ["/my-profile", "/update-profile", "/details-page/:path*"],
 };
